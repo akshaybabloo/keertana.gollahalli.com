9b3e8d34d4a959a7bf6f3dc6a771581d98eee195
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2020-04-22 17:51:38 +1200

    libs inti

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const {src, dest, parallel} = require('gulp');
+
+// Following are the default paths that the files will be moved to
+const cssFolderPath = './themes/Spark/assets/css/';
+const jsFolderPath = './themes/Spark/assets/js/';
+const fontPath = './themes/Spark/static/font';
+
+// Following are the files that will be moved to their locations
+const cssFilesToMove = [];
+const jsFilesToMove = [
+    './node_modules/instantsearch.js/dist/instantsearch.development.js',
+    './node_modules/algoliasearch/dist/algoliasearch.umd.js',
+    './node_modules/instantsearch.js/dist/instantsearch.production.min.js',
+    './node_modules/algoliasearch/dist/algoliasearch.umd.js'];
+const fontFilesToMove = [
+    './node_modules/firacode/distr/woff/FiraCode-Regular.woff',
+    './node_modules/firacode/distr/woff2/FiraCode-Regular.woff2'
+];
+
+// Moves CSS files to their appropriate location
+function moveCss() {
+    return src(cssFilesToMove).pipe(dest(cssFolderPath));
+}
+
+// Moves JS files to their appropriate location
+function moveJs() {
+    return src(jsFilesToMove).pipe(dest(jsFolderPath));
+}
+
+// Moves JS files to their appropriate location
+function moveFont() {
+    return src(fontFilesToMove).pipe(dest(fontPath));
+}
+
+// Run both functions in parallel
+exports.default = parallel(moveCss, moveJs, moveFont);
