eb48aba009d7083f82d1e4040202109bb32b3262
Akshay Raj Gollahalli <akshay@gollahalli.com>
2026-01-26 15:16:18 +1300

    Update package and fix seo

--- themes/Indigo/src/app.ts
+++ themes/Indigo/src/app.ts
@@ -4,6 +4,29 @@ import "@fontsource/lato";
 
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin";
+import { 
+    faBars, 
+    faTimes, 
+    faEnvelope, 
+    faBriefcase, 
+    faGraduationCap, 
+    faCertificate, 
+    faCheckCircle,
+    faUserMd,
+    faTooth 
+} from "@fortawesome/free-solid-svg-icons";
 
-library.add(faLinkedin);
+library.add(
+    faLinkedin, 
+    faBars, 
+    faTimes, 
+    faEnvelope, 
+    faBriefcase, 
+    faGraduationCap, 
+    faCertificate, 
+    faCheckCircle,
+    faUserMd,
+    faTooth
+);
+
 dom.watch();
